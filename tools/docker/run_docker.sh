18a9c2879e480e34f4c19da45b9c3d27547c4f06
Sheikh Dawood <7774242+sheikh-nv@users.noreply.github.com>
2026-01-14 10:35:45 -0600

    Fix run_docker.sh

--- tools/docker/run_docker.sh
+++ tools/docker/run_docker.sh
@@ -2,5 +2,5 @@
 PRIVACY_EMAIL="${PRIVACY_EMAIL:-user@example.com}"  # Allow override via environment
 
 docker run --name isaac-sim --entrypoint bash -it --gpus all -e "ACCEPT_EULA=Y" --rm \
- --network=host -e "PRIVACY_CONSENT=Y" -e "PRIVACY_USERID=${PRIVACY_EMAIL}" isaac-sim-docker:latest \
+ --network=host -u 1234:1234 -e "PRIVACY_CONSENT=Y" -e "PRIVACY_USERID=${PRIVACY_EMAIL}" isaac-sim-docker:latest \
  "$@"
